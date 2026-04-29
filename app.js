a2f4a4f07343776ac5a945f06fb02a2312d024aa
PatilManas3333 <patilmanas3333@gmail.com>
2026-04-29 05:46:10 +0530

    Fix: bind app to 0.0.0.0 for Docker

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+// Route
 app.get('/', (req, res) => {
     res.send('🚀 CI/CD Pipeline is Working!');
 });
 
-app.listen(PORT, () => {
+// IMPORTANT: bind to 0.0.0.0 for Docker
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
